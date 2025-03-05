962e5758a39d54f2adae594b39ebce58fdd5d523
NRaykov <nediotenev@gmail.com>
2025-03-05 23:19:13 +0200

    Add router-outlet. Setup routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductsListComponent],
-  template: '<app-header></app-header><app-products-list />',
+  imports: [HeaderComponent, RouterOutlet],
+  template: '<app-header></app-header><router-outlet />',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
