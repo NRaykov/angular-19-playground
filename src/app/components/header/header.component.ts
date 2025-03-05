94129303056bb2cfa5dd94b0371113794bc0761a
NRaykov <nediotenev@gmail.com>
2025-03-05 18:54:07 +0200

    Create primary-button component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  title = signal('Angular Store');
 
+  cart = signal('Cart');
+
+  showButtonEmit() {
+    console.log('clicked');
+  }
+  
 }
