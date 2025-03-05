69a0514a79124968e52cc590af5b1dd57df57ea3
NRaykov <nediotenev@gmail.com>
2025-03-05 23:49:37 +0200

    Create shopping cart component

---
+++ src/app/pages/cart-item/cart-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Product } from '../../models/products.model';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss'
+})
+export class CartItemComponent {
+
+  product = input.required<Product>();
+
+}
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,7 +6,22 @@ import { Product } from '../models/products.model';
 })
 export class CartService {
 
-  cart = signal<Product[]>([]);
+  cart = signal<Product[]>([
+    {
+      id: 1,
+      title: 'Red Handbag',
+      image: 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      price: 100,
+      stock: 10
+    },
+    {
+      id: 2,
+      title: 'Brown Handbag - Leather',
+      image: 'https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6?q=80&w=1963&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      price: 200,
+      stock: 100
+    },
+  ]);
 
   addToCart(product: Product) {
     this.cart.set([...this.cart(), product]); 
